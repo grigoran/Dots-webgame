49c8a03ffc245f8eca9874b9ba8c29f87a490249
grigoran <igormin81@mail.ru>
2021-04-10 20:56:36 +0400

    feat: complete welcome popup styles

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 //document.querySelector(".welcome-popup").classList.toggle("welcome-popup_hiden")
-let welcome_popup=document.querySelector(".welcome-popup");
-let welcome_popup__form=welcome_popup.querySelector(".welcome-popup__form")
+let welcome_popup = document.querySelector(".welcome-popup");
+let welcome_popup__form = welcome_popup.querySelector(".welcome-popup__form");
 
-window.onload=loadHandler;
-welcome_popup__form.addEventListener("submit",welcomeFormSubmit);
+window.onload = loadHandler;
+welcome_popup__form.addEventListener("submit", welcomeFormSubmit);
 
-function loadHandler(){
-    setTimeout(()=>welcome_popup.classList.toggle("welcome-popup_shown"),150);   
+function loadHandler() {
+  setTimeout(() => welcome_popup.classList.toggle("welcome-popup_shown"), 150);
 }
 
-
-function welcomeFormSubmit(event){
-    welcome_popup.classList.toggle("welcome-popup_shown");
-    event.preventDefault();
+function welcomeFormSubmit(event) {
+  welcome_popup.classList.toggle("welcome-popup_shown");
+  setTimeout(() => welcome_popup.classList.toggle("welcome-popup_hidden"), 150);
+  event.preventDefault();
 }
