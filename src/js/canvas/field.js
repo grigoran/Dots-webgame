18d528e1ec755604a1316d8c1d052ddfe999171b
grigoran <igormin81@mail.ru>
2021-04-25 16:10:16 +0400

    rem: replace separate exports with one export using Field class

--- src/js/canvas/field.js
+++ src/js/canvas/field.js
@@ -1,30 +1,31 @@
 import { canvas, ctx } from "./init-canvas.js";
 
-export let field = {
-  size: {
-    x: 15,
-    y: 15,
-  },
-};
+class Field {
+  constructor() {
+    //Размер поля
+    this.size = {
+      x: 15,
+      y: 15,
+    };
 
-function getDrawFieldFunc() {
-  let path = new Path2D();
-  let step = canvas.width / field.size.x;
+    let path = new Path2D(); //Путь для рисования поля
+    let step = canvas.width / this.size.x; //шаг сетки
 
-  for (let i = 0; i < field.size.x; i++) {
-    path.moveTo(i * step + step / 2, 0);
-    path.lineTo(i * step + step / 2, canvas.height);
+    for (let i = 0; i < this.size.x; i++) {
+      path.moveTo(i * step + step / 2, 0);
+      path.lineTo(i * step + step / 2, canvas.height);
+    }
+    for (let i = 0; i < this.size.y; i++) {
+      path.moveTo(0, i * step + step / 2);
+      path.lineTo(canvas.width, i * step + step / 2);
+    }
+
+    this.drawField = function () {
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.stroke(path);
+    };
   }
-  for (let i = 0; i < field.size.y; i++) {
-    path.moveTo(0, i * step + step / 2);
-    path.lineTo(canvas.width, i * step + step / 2);
-  }
-
-  return function () {
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-    ctx.stroke(path);
-  };
 }
 
-export let drawField = getDrawFieldFunc();
+export let field = new Field();
--- src/js/canvas/stages/game-stage.js
+++ src/js/canvas/stages/game-stage.js
@@ -1,7 +1,7 @@
 import { canvas, ctx } from "./../init-canvas.js";
 import { localPlayerNickname, localPlayerColor } from "../../welcome-form.js";
 import { Stage } from "./stage.js";
-import { drawField } from "../field.js";
+import { field } from "../field.js";
 
 import memImage from "../../../img/mike.jpg";
 
@@ -29,7 +29,7 @@ function drawCursor() {
 
 export class GameStage extends Stage {
   update(deltaTime) {
-    drawField();
+    field.drawField();
     drawCursor();
   }
 }
