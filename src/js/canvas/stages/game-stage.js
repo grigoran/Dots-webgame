bcd9ad8dc9814208b06a1d0d49efde6c8825df3e
grigoran <igormin81@mail.ru>
2021-04-25 13:54:11 +0400

    feat: add cursor movements

--- src/js/canvas/stages/game-stage.js
+++ src/js/canvas/stages/game-stage.js
@@ -5,8 +5,31 @@ import { drawField } from "../field.js";
 
 import memImage from "../../../img/mike.jpg";
 
+document.addEventListener("mousemove", mouseMoveHandler);
+
+let mousePos = {
+  x: 0,
+  y: 0,
+};
+
+function mouseMoveHandler(event) {
+  let rect = canvas.getBoundingClientRect();
+  let scaleX = canvas.width / rect.width;
+  let scaleY = canvas.height / rect.height;
+  mousePos.x = (event.clientX - rect.left) * scaleX;
+  mousePos.y = (event.clientY - rect.top) * scaleY;
+}
+
+function drawCursor() {
+  ctx.beginPath();
+  ctx.fillStyle = localPlayerColor;
+  ctx.arc(mousePos.x, mousePos.y, 10, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
 export class GameStage extends Stage {
   update(deltaTime) {
     drawField();
+    drawCursor();
   }
 }
