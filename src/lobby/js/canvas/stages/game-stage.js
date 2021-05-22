32bfdf5d5072cbcccbc6d482d0db130d077bf8e6
grigoran <igormin81@mail.ru>
2021-05-22 18:52:23 +0400

    turn off dev mode

--- src/lobby/js/canvas/stages/game-stage.js
+++ src/lobby/js/canvas/stages/game-stage.js
@@ -11,7 +11,7 @@ let mousePos = new Vector();
 
 let localTurn = false; //ходит ли сейчас локальный игрок
 
-const DEV_MODE = true;
+const DEV_MODE = false;
 
 gameServer.onPlace((pos) => {
   field.placeDotDirect(pos, player.remote.color);
